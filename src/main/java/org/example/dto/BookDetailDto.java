03b47ff182870c84fcaa83fcb1c8bd3a1f7a9f70
tagayalexandra <Tagayaleksandra@gmail.com>
2025-04-05 12:59:30 +0300

    - Added validation to WorkSpaceDto and BookDetailDto. - Added interfaces for group validation. - Added web configuration;

--- src/main/java/org/example/dto/BookDetailDto.java
+++ src/main/java/org/example/dto/BookDetailDto.java
@@ -1,5 +1,8 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import org.example.entity.WorkSpace;
 
@@ -9,18 +12,21 @@ import java.time.LocalTime;
 @Data
 public class BookDetailDto {
 
-
+    @NotBlank(message = "ID is required, cannot be empty", groups = OnUpdate.class)
+    @Positive(message = "ID must be a positive number")
     private Long id;
 
-
+    @NotBlank(message = "Data is required, cannot be empty", groups = {OnCreate.class, OnUpdate.class})
+    @Future(message = "Data should be in the future")
     private LocalDate date;
 
+    @NotBlank(message = "Start time  is required, cannot be empty", groups = {OnCreate.class, OnUpdate.class})
     private LocalTime startTime;
 
-
+    @NotBlank(message = "End time is required, cannot be empty", groups = {OnCreate.class, OnUpdate.class})
     private LocalTime endTime;
 
-
+    @NotBlank
     private WorkSpace workSpace;
 
 
--- src/main/java/org/example/dto/WorkSpaceDto.java
+++ src/main/java/org/example/dto/WorkSpaceDto.java
@@ -1,5 +1,8 @@
 package org.example.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,16 +10,17 @@ import java.math.BigDecimal;
 @Data
 public class WorkSpaceDto {
 
-
+    @NotBlank(message = "ID is required, cannot be empty", groups = OnUpdate.class)
+    @Positive(message = "ID must be a positive number")
     private Long id;
 
-
+    @NotBlank(message = "TypeSpace is required, cannot be empty", groups = {OnCreate.class, OnUpdate.class})
     private String typeSpace;
 
-
+    @NotBlank(message = "The price is required, cannot be empty", groups = {OnCreate.class, OnUpdate.class})
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
     private BigDecimal price;
 
-
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
     private boolean available;
-
 }
